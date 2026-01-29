ba51ea2f90af4521663df66337a327856e5f2445
gllflplaystore <gllfl.playstore@gmail.com>
2026-01-29 17:04:56 +0530

    done new repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,22 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("maven-publish")
 }
 
 android {
     namespace = "com.omnissolutio.paymentsdk"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildFeatures {
+        compose = true
     }
 
     buildTypes {
@@ -25,10 +28,12 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
@@ -38,7 +43,42 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    // Compose (safe for SDKs)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+/* =======================
+   PUBLISH CONFIG
+   ======================= */
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.omnissolutio"
+            artifactId = "omnis-solutio-payment-sdk-android"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/gllflplaystore/omnis-solutio-payment-sdk-android")
+
+            credentials {
+                username = (project.findProperty("gpr.user") as String?) ?: System.getenv("GITHUB_USER")
+                password = (project.findProperty("gpr.token") as String?) ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
